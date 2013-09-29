b9dadc58f637c1cdfe54ae0ae028b17f38145d1f
verkholantsev <poshli.pismo@gmail.com>
2013-09-30 00:18:42 +0400

    Hashes save in local storage

--- missword.js
+++ missword.js
@@ -1,16 +1,28 @@
 (function (window, CryptoJS, undefined) {
+	var _Local = function () {
+		this.storage = window.localStorage;
+	};
+
+	_Local.prototype.add = function (hash, url) {
+		this.storage.setItem(url, hash);
+	}
+
+	_Local.prototype.get = function (url) {
+		return this.storage.getItem(url);
+	}
+
 	var Missword = window.Missword = function (settings) {
 		this.master = settings.master;
-		this.storage = {};
-	}
+		this.storage = new _Local();
+	};
 
 	Missword.prototype.add = function (password, url) {
 		var str = url + password;
-		this.storage[url] = CryptoJS.AES.encrypt(str, this.master).toString();
-	}
+		this.storage.add(CryptoJS.AES.encrypt(str, this.master).toString(), url);
+	};
 
 	Missword.prototype.get = function(url) {
-		var pass = CryptoJS.AES.decrypt(this.storage[url], this.master)
+		var pass = CryptoJS.AES.decrypt(this.storage.get(url), this.master)
 			.toString(CryptoJS.enc.Utf8)
 			.slice(url.length);
 
