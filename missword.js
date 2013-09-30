4a51c91d007469b0dd6841f109137c195c20839c
verkholantsev <poshli.pismo@gmail.com>
2013-09-30 20:43:41 +0400

    First view implementation

--- missword.js
+++ missword.js
@@ -5,15 +5,46 @@
 
 	_Local.prototype.add = function (hash, url) {
 		this.storage.setItem(url, hash);
-	}
+	};
 
 	_Local.prototype.get = function (url) {
 		return this.storage.getItem(url);
-	}
+	};
+
+	_Local.prototype.keys = function () {
+		return Object.keys(window.localStorage);
+	};
+
+
+	_View = function (settings) {
+		this.model = settings.model;
+		this.container = settings.container;
+
+		this.render();
+	};
+
+	_View.prototype.render = function () {
+		if (this.container) {
+			var keys = this.model.keys();
+			var _this = this;
+			keys.forEach(function (key) {
+				_this.container.append(
+					'<div class="pass">' +
+						'<span>' + key + '</span>' +
+						'<input value=' + _this.model.get(key) + '>' +
+					'</div>'
+				);
+			});
+		}
+	};
 
 	var Missword = window.Missword = function (settings) {
 		this.master = settings.master;
 		this.storage = new _Local();
+		this.view = new _View({
+			container: settings.container,
+			model: this
+		});
 	};
 
 	Missword.prototype.add = function (password, url) {
@@ -21,11 +52,15 @@
 		this.storage.add(CryptoJS.AES.encrypt(str, this.master).toString(), url);
 	};
 
-	Missword.prototype.get = function(url) {
+	Missword.prototype.get = function (url) {
 		var pass = CryptoJS.AES.decrypt(this.storage.get(url), this.master)
 			.toString(CryptoJS.enc.Utf8)
 			.slice(url.length);
 
 		return pass;
 	};
+
+	Missword.prototype.keys = function () {
+		return this.storage.keys();
+	}
 })(window, CryptoJS);
--- test.js
+++ test.js
@@ -1,7 +1,10 @@
 (function (window, Missword, undefined) {
 	
 	window.onload = function () {
-		var m = new Missword({master: 'master-password'});
+		var m = new Missword({
+			master: 'master-password',
+			container: $(window.document.body)
+		});
 
 		m.add('first-password', 'example.com');
 		m.add('second-password', 'http://example.com');
@@ -10,6 +13,6 @@
 		console.log(m.get('example.com'));
 		console.log(m.get('http://example.com'));
 		console.log(m.get('http://example.com/somepage'));
-	}
+	};
 
 })(window, Missword);
